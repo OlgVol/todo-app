3435ecc61737528b128b69605852d0b017a6234f
Volkova <olga.volkova@accenture.com>
2023-07-04 16:11:56 +0300

    created service to render tasks from json server

--- src/app/task-card/task-card.component.ts
+++ src/app/task-card/task-card.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ITask } from '../models/task.model';
+import { TaskServiceService } from '../shared/task-service/task-service.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-task-card',
@@ -8,4 +10,19 @@ import { ITask } from '../models/task.model';
 })
 export class TaskCardComponent {
 tasks: ITask[] = [];
+sub!: Subscription;
+
+constructor(
+  private taskService: TaskServiceService
+) {
+
+}
+
+ngOnInit() {
+  this.sub = this.taskService.getTasks().subscribe({
+    next: (tasks) => {
+      this.tasks = tasks;
+    },
+  });
+}
 }
